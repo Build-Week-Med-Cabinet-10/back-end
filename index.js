d673d285c29a8188c734eaa1eac539598ae2600c
thomas <t_huynh927@yahoo.com>
2020-06-17 22:19:12 -0700

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-require('dotenv').config({ path: './config/.env'})
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 server.listen(PORT, () => {
   console.log(`\n<(^-^<) Server listening on port ${PORT} (>^-^)>`);
 });
